e1920b33c88f913412711a100dc30ece7021b15e
Timberclaw <131182894+TimberclawDev@users.noreply.github.com>
2024-04-18 23:57:18 +0200

    Added java download for all linux distributions

--- smoothcloud-start.sh
+++ smoothcloud-start.sh
@@ -7,7 +7,18 @@ case $JAVA_VER in
         read -p "Do you wish to update java? (Yy/Nn): " yn
         case $yn in
         [Yy]*)
-          sudo apt install default-jre
+          wget https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.deb
+          sudo dpkg -i jdk-22_linux-x64_bin.deb
+          java --version
+
+          ls -ld /usr/lib/jvm/jdk*
+          sudo nano /etc/environment
+
+          export JAVA_HOME="/usr/lib/jvm/jdk-22-oracle-x64"
+          export PATH="$PATH:${JAVA_HOME}/bin"
+
+          source  /etc/environment
+          echo $JAVA_HOME
           break;;
         [Nn]*)
           echo "Aborting setup!"
@@ -27,7 +38,18 @@ $BIN*)
     read -p "Java is not installed. Do you wish to install it? (Yy/Nn): " yn
     case $yn in
     [Yy]*)
-      sudo apt install default-jre
+      wget https://download.oracle.com/java/22/latest/jdk-22_linux-x64_bin.deb
+      sudo dpkg -i jdk-22_linux-x64_bin.deb
+      java --version
+
+      ls -ld /usr/lib/jvm/jdk*
+      sudo nano /etc/environment
+
+      export JAVA_HOME="/usr/lib/jvm/jdk-22-oracle-x64"
+      export PATH="$PATH:${JAVA_HOME}/bin"
+
+      source  /etc/environment
+      echo $JAVA_HOME
       break;;
     [Nn]*)
       echo "Aborting setup!"
